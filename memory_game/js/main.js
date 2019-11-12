a5efd03161b5d3ad92a29a3a11da28005c556356
Christopher Greenwood <greenwood.cm88@gmail.com>
2019-11-11 22:46:19 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,19 +3,24 @@
 const cards = ['queen', 'queen', 'king', 'king'];
 const cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkforMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 		}
-	
 }
+
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkforMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
+
+
